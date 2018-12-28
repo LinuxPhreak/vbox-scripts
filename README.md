fea9108a7360f4d7483d2041f147e060f8f7a075
LinuxPhreak <linuxphreak@localhost.localdomain>
2018-12-28 06:18:24 -0500

    First Pages commit

--- README.md
+++
@@ -1,4 +0,0 @@
-# VirtualBox Scripts
-Different Scripts for VirtualBox to make my life easier
-
-All files are tested using VirtualBox 6 on Fedora 28 Linux.
--- macos-mojave.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-VBoxManage modifyvm "Mac OSX Mojave" --cpuidset 00000001 000106e5 00100800 0098e3fd bfebfbff
-VBoxManage setextradata "Mac OSX Mojave" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "iMac11,3"
-VBoxManage setextradata "Mac OSX Mojave" "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
-VBoxManage setextradata "Mac OSX Mojave" "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Iloveapple"
-VBoxManage setextradata "Mac OSX Mojave" "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
-VBoxManage setextradata "Mac OSX Mojave" "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
