1e5d236872dda40a38985accbbb2cca20df312fc
LinuxPhreak <linuxphreak@localhost.localdomain>
2018-12-28 04:50:23 -0500

    Changed the badly formatted quotation marks. Also tested and it works just fine now

--- resolution.sh
+++ resolution.sh
@@ -12,33 +12,33 @@ if [ $noyes == "y" -o $noyes == "Y" ]; then
 
 		case $choice in
 			'1920x1080')
-				VBoxManage setextradata “$name” VBoxInternal2/EfiGraphicsResolution 1920x1080
+				VBoxManage setextradata "$name" VBoxInternal2/EfiGraphicsResolution 1920x1080
 				;;
 			'2048x1080')
-				VBoxManage setextradata “$name” VBoxInternal2/EfiGraphicsResolution 2048x1080
+				VBoxManage setextradata "$name" VBoxInternal2/EfiGraphicsResolution 2048x1080
 				;;
 			'2560x1440')
-				VBoxManage setextradata “$name” VBoxInternal2/EfiGraphicsResolution 2560x1440
+				VBoxManage setextradata "$name" VBoxInternal2/EfiGraphicsResolution 2560x1440
 				;;
 			'3840x2160')
-				VBoxManage setextradata “$name” VBoxInternal2/EfiGraphicsResolution 3840x2160
+				VBoxManage setextradata "$name" VBoxInternal2/EfiGraphicsResolution 3840x2160
 				;;
 			'1280x800')
-				VBoxManage setextradata “$name” VBoxInternal2/EfiGraphicsResolution 1280x800
+				VBoxManage setextradata "$name" VBoxInternal2/EfiGraphicsResolution 1280x800
 				;; 
 			'1280x1024')
-				VBoxManage setextradata “$name” VBoxInternal2/EfiGraphicsResolution 1280x1024
+				VBoxManage setextradata "$name" VBoxInternal2/EfiGraphicsResolution 1280x1024
 				;;
 			'1440x900') 
-				VBoxManage setextradata “$name” VBoxInternal2/EfiGraphicsResolution 1440x900
+				VBoxManage setextradata "$name" VBoxInternal2/EfiGraphicsResolution 1440x900
 				;;
 			'1600x900')
-				VBoxManage setextradata “$name” VBoxInternal2/EfiGraphicsResolution 1600x900
+				VBoxManage setextradata "$name" VBoxInternal2/EfiGraphicsResolution 1600x900
 				;;
 		esac
 		break
 	done
-	#VBoxManage setextradata “$name” VBoxInternal2/EfiGraphicsResolution X
+	#VBoxManage setextradata "$name" VBoxInternal2/EfiGraphicsResolution X
 	#1280x720, 1920x1080, 2048x1080, 2560x1440, 3840x2160, 1280x800, 1280x1024, 1440x900, 1600x900
 	#VirtualBox doesn't support: 1366x768 (you can use 1280x720 with "Scaled Mode"). 
 fi
