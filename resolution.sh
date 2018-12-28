be0a92926e5379fbf48cef45478cb3537a7f3d34
LinuxPhreak <linuxphreak@localhost.localdomain>
2018-12-28 02:16:21 -0500

    Added comments to the resolution.sh

--- resolution.sh
+++ resolution.sh
@@ -7,5 +7,7 @@ read name
 echo -e "Your VM Name is $name correct? (y/N):"
 read noyes
 if [ $noyes == "y" -o $noyes == "Y" ]; then
-	#VBoxManage setextradata "Mac OSX High Sierra" VBoxInternal2/EfiGraphicsResolution 1600x900
+	#VBoxManage setextradata “Your Virtual Machine Name” VBoxInternal2/EfiGraphicsResolution X
+	#1280x720, 1920x1080, 2048x1080, 2560x1440, 3840x2160, 1280x800, 1280x1024, 1440x900, 1600x900
+	#VirtualBox doesn't support: 1366x768 (you can use 1280x720 with "Scaled Mode"). 
 fi
