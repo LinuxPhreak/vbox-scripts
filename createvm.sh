d27afb65e70435a0bc2469ee4c4f54e320378d22
LinuxPhreak <linuxphreak@localhost.localdomain>
2018-12-30 22:06:35 -0500

    Add mkdir feature and check if VBox default directory exists

--- createvm.sh
+++ createvm.sh
@@ -1,4 +1,7 @@
  #!/usr/bin/env sh
+vboxdir="$HOME/VirtualBox VMs"
+if [ -d "$vboxdir" ]; then
+
 ostypes=('Other' 'Other_64' 'Windows31' 'Windows95' 'Windows98' 'Windows2000' 'WindowsMe' 'WindowsXP' 'WindowsXP_64' 'Windows2003' 'WindowsNT3x' 'WindowsNT4' 'WindowsNT_64' 'WindowsNT' 'WindowsVista' 'WindowsVista_64' 'Windows2008' 'Windows7' 'Windows7_64' 'Windows8' 'Windows8_64' 'Windows81' 'Windows81_64' 'Windows2012_64' 'Windows10' 'Windows10_64' 'Windows2016_64' 'Linux22' 'Linux24' 'Linux24_64' 'Linux26' 'Linux26_64' 'ArchLinux' 'ArchLinux_64' 'Debian' 'Debian_64' 'Fedora' 'Fedora_64' 'Gentoo' 'Gentoo_64' 'Mandriva' 'Mandriva_64' 'Oracle' 'Oracle_64' 'RedHat' 'RedHat_64' 'OpenSUSE' 'OpenSUSE_64' 'Turbolinux' 'Turbolinux_64' 'Ubuntu' 'Ubuntu_64' 'Xandros' 'Xandros_64' 'Solaris' 'Solaris_64' 'Solaris11_64' 'OpenSolaris' 'OpenSolaris_64' 'FreeBSD' 'FreeBSD_64' 'OpenBSD' 'OpenBSD_64' 'NetBSD' 'NetBSD_64' 'OS2Warp3' 'OS2Warp4' 'OS2Warp45' 'OS2eCS' 'OS21x' 'OS2' 'MacOS' 'MacOS_64' 'MacOS106' 'MacOS106_64' 'MacOS107_64' 'MacOS108_64' 'MacOS109_64' 'MacOS1010_64' 'MacOS1011_64' 'MacOS1012_64' 'MacOS1013_64' 'DOS' 'Netware' 'L4' 'QNX' 'JRockitVE' 'VBoxBS_64')
 echo -e "Enter the number for the Operating System you wish to install..."
 select ostype in "${ostypes[@]}"; do
@@ -8,9 +11,10 @@ select ostype in "${ostypes[@]}"; do
  			echo -e "Operating System Type Other"
  			echo -e "What do you want to call this VM?"
  			read name
+ 			mkdir -p "$vboxdir/$name"
  			while [ x$no != "N" -o x$no != "n" ]
 			do
- 				echo -e "How much memory do you want to givw your VM?"
+ 				echo -e "How much memory do you want to give your VM?"
  				read memory
  				echo -e "You want to set $memory MB for your VM? Is this correct? (y/N)"
 				read no
@@ -28,8 +32,8 @@ select ostype in "${ostypes[@]}"; do
  			fi
  			VBoxManage createvm --name "$name" --ostype Other --register
  			VBoxManage modifyvm "$name" --memory $memory
- 			VBoxManage createhd --filename "$name.vdi" --size $hdsize --format VDI
-			VBoxManage storageattach "$name" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$name.vdi"
+ 			VBoxManage createhd --filename "$vboxdir/$name/$name.vdi" --size $hdsize --format VDI
+			VBoxManage storageattach "$name" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$vboxdir/$name/$name.vdi"
 			VBoxManage storagectl "$name" --name "SATA Controller" --add sata --controller IntelAhci
 			VBoxManage storagectl "$name" --name "IDE Controller" --add ide --controller PIIX4									
  			;;
@@ -88,6 +92,9 @@ select ostype in "${ostypes[@]}"; do
  	esac
 	break
 done
+else
+	echo "The directory $vboxdir does not exists and needs to exists for this script to work."
+fi
 #VBoxManage createvm --name "FBSD Test" --ostype FreeBSD_64 --register
 #VBoxManage modifyvm testMachine --memory 256
 #VBoxManage createhd --filename testMachine.vdi --size 18000 --format
