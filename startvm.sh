fe7279878768e883dbac284261a0b6071d2cddb5
Ben P. Dorsi-Todaro <ben@bigbenshosting.com>
2018-12-31 21:08:36 -0500

    added running options to startvm

--- startvm.sh
+++ startvm.sh
@@ -1,4 +1,37 @@
 #!/usr/bin/env sh
 echo -e "Type the name of the virtual machine you would like to start."
 read vmname
-VBoxManage startvm "$vmname" &
+VBoxManage startvm "$vmname"
+controloptions=('Off' 'Shutdown' 'Pause' 'Reset' 'Resume')
+echo -e "Enter A Number to control the VM"
+
+select vmcontrol in "${controloptions[@]}"; do
+	[[ -n $vmcontrol ]] || { echo "Invalid choice." >&2; continue; }
+	case $vmcontrol in
+		'Off')
+			VBoxManage controlvm "$vmname" poweroff
+			;;
+		'Shutdown')
+			VBoxManage controlvm "$vmname" poweroff soft
+			;;
+		'Pause')
+			VBoxManage controlvm "$vmname" pause
+			while [ x$resume != "r" -o x$resume != "R" ]
+			do
+				echo "Press R to resume machine state"
+				read resume
+				if [ $resume == "r" -o $resume == "R" ]; then
+					VBoxManage controlvm "$vmname" resume
+					break
+				fi
+			done
+			;;
+		'Reset')
+			VBoxManage controlvm "$vmname" reset
+			;;
+		'Resume')
+			VBoxManage controlvm "$vmname" resume
+			;;
+ 	esac
+	break
+done	
