910e24951d1af4eba6c9dc149357a730a45f6d35
Ben P. Dorsi-Todaro <ben@bigbenshosting.com>
2019-01-01 04:46:48 -0500

    Working on test shortcut script

---
+++ test-shortcut-shell.sh
@@ -0,0 +1,23 @@
+#/usr/bin/env sh
+# This is a shell script to fix the problem VirtualBox 6.0.0 has when creating Desktop Shortcuts on Linux systems. 
+# This shell script has been tested on Fedora 28 LXDE
+# Bug report https://www.virtualbox.org/ticket/18259 LinuxPhreak2600 aka LinuxPhreak aka Ben P. Dorsi-Todaro
+# This shell script is provided by Ben P. Dorsi-Todaro
+echo -e "Which Virtual Machine do you want to make a Desktop Shortcut for?"
+read vmname
+
+#if [[ -d "$HOME/VirtualBox VMs" ]]; then
+if [[ "`VBoxManage list vms | grep '$vmname'`" != "" ]]; then
+	echo -e "[Desktop Entry]" >> $HOME/Desktop/$vmname.Desktop
+	echo -e "Encoding=UTF-8" >> $HOME/Desktop/$vmname.Desktop
+	echo -e "Version=1.0" >> $HOME/Desktop/$vmname.Desktop
+	echo -e "Name=$vmname" >> $HOME/Desktop/$vmname.Desktop
+	echo -e "Comment=Starts the VirtualBox machine $vmname" >> $HOME/Desktop/$vmname.Desktop
+	echo -e "Type=Application" >> $HOME/Desktop/$vmname.Desktop
+	echo -e "Exec=VBoxManage startvm '$vmname' %u" >> $HOME/Desktop/$vmname.Desktop
+	echo -e "Icon=virtualbox-vbox.png" >> $HOME/Desktop/$vmname.Desktop
+	chmod +x "$HOME/Desktop/$vmname.Desktop"
+	echo -e "The shortcut was successfully created..."
+else
+	echo -e "Sorry it doesn't appear the Virtual Machine exists"
+fi
