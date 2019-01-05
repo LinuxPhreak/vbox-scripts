950d39a7e918743488f2df272a5ef82ecc200c5b
Ben P. Dorsi-Todaro <ben@bigbenshosting.com>
2019-01-05 02:29:16 -0500

    Untested GUI in python. GUI will be the wrapper for the new createvm.sh

--- testing-createvm.sh
+++ testing-createvm.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 oses="ArchLinux|ArchLinux_64|Debian|Debian_64|DOS|Fedora|Fedora_64|FreeBSD|FreeBSD_64|Gentoo|Gentoo_64|JRockitVE|L4|Linux22|Linux24|Linux24_64|Linux26|Linux26_64|MacOS|MacOS1010_64|MacOS1011_64|MacOS1012_64|MacOS1013_64|MacOS106|MacOS106_64|MacOS107_64|MacOS108_64|MacOS109_64|MacOS_64|Mandriva|Mandriva_64|NetBSD|NetBSD_64|Netware|OpenBSD|OpenBSD_64|OpenSolaris|OpenSolaris_64|OpenSUSE|OpenSUSE_64|Oracle|Oracle_64|OS2|OS21x|OS2eCS|OS2Warp3|OS2Warp4|OS2Warp45|Other|Other_64|QNX|RedHat|RedHat_64|Solaris|Solaris11_64|Solaris_64|Turbolinux|Turbolinux_64|Ubuntu|Ubuntu_64|VBoxBS_64Windows10|Windows10_64|Windows2000|Windows2003|Windows2008|Windows2012_64|Windows2016_64|Windows31|Windows7|Windows7_64|Windows8|Windows81|Windows81_64|Windows8_64|Windows95|Windows98|WindowsMe|WindowsNT|WindowsNT3x|WindowsNT4|WindowsNT_64|WindowsVista|WindowsVista_64|WindowsXP|WindowsXP_64|Xandros|Xandros_64"
+
 vboxdir="$HOME/VirtualBox VMs"
+
 usuage() 
 { 
 	echo "Usuage:"
@@ -17,7 +19,6 @@ while getopts ":n:a:m:s:" o; do
 		n=${OPTARG}
 		;;
     a)
-      #echo "-a was triggered!" >&2
 		a=${OPTARG}
 		;;
 	m)
